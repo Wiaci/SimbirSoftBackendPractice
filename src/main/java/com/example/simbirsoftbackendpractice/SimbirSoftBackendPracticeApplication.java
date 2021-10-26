548fd70ce465a05c46e957a752e4ef5ad8d3b1aa
Wiaci <vyachproz7@gmail.com>
2021-10-26 16:17:56 +0300

    first commit

---
+++ src/main/java/com/example/simbirsoftbackendpractice/SimbirSoftBackendPracticeApplication.java
@@ -0,0 +1,13 @@
+package com.example.simbirsoftbackendpractice;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SimbirSoftBackendPracticeApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SimbirSoftBackendPracticeApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/example/simbirsoftbackendpractice/SimbirSoftBackendPracticeApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.simbirsoftbackendpractice;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SimbirSoftBackendPracticeApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
